348074341e71b7c0c05ea8bacdb77cac78ad94e9
Joao Moreno <mail@joaomoreno.com>
2014-11-24 17:16:28 +0100

    fixed build for windows

--- src/win.js
+++ src/win.js
@@ -8,25 +8,28 @@ exports.getAppPath = function(productName) {
 	return 'resources/app';
 };
 
+function patchExecutable(executablePath, productName, productVersion, cb) {
+	var versionString = {
+		CompanyName: 'GitHub, Inc.',
+		FileDescription: 'Atom',
+		LegalCopyright: 'Copyright (C) 2014 GitHub, Inc. All rights reserved',
+		ProductName: productName,
+		ProductVersion: productVersion
+	};
+	
+	rcedit(executablePath, { 'version-string': versionString }, cb);
+}
+
 exports.patchAtom = function(opts, cb) {
 	var p = function (path) { return opts.outputPath + '/' + path; };
 
 	rimraf(p('resources/default_app'), function (err) {
 		if (err) { return cb(err); }
 
-		var executablePath = p('atom.exe');
-		var versionString = {
-			CompanyName: 'GitHub, Inc.',
-			FileDescription: 'Atom',
-			LegalCopyright: 'Copyright (C) 2014 GitHub, Inc. All rights reserved',
-			ProductName: opts.productName,
-			ProductVersion: opts.productVersion
-		};
-		
-		rcedit(p('atom.exe'), { 'version-string': versionString }, function (err) {
+		patchExecutable(p('atom.exe'), opts.productName, opts.productVersion, function (err) {
 			if (err) { return cb(err); }
 
-			fs.rename(p('atom.exe'), p(opts.productName + '.exe'), cb);	
+			fs.rename(p('atom.exe'), p(opts.productName + '.exe'), cb);
 		});
 	});
 };
