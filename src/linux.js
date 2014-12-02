c41f5667aef7725685880d6b8a5d7a2bd2c31c75
Joao Moreno <mail@joaomoreno.com>
2014-12-02 17:30:17 +0100

    linux: make the exe executable

--- src/linux.js
+++ src/linux.js
@@ -3,6 +3,13 @@
 var fs = require('fs');
 var rimraf = require('rimraf');
 
+function setExecutableBit(path, cb) {
+	fs.stat(path, function (err, stats) {
+		if (err) { return cb(err); }
+		fs.chmod(path, stats.mode | 100, cb);
+	});
+}
+
 exports.getAppPath = function(productName) {
 	return 'resources/app';
 };
@@ -13,6 +20,10 @@ exports.patchAtom = function(opts, cb) {
 	rimraf(p('resources/default_app'), function (err) {
 		if (err) { return cb(err); }
 
-		fs.rename(p('atom'), p(opts.productName), cb);
+		fs.rename(p('atom'), p(opts.productName), function (err) {
+			if (err) { return cb(err); }
+
+			setExecutableBit(p(opts.productName), cb);
+		});
 	});
 };
