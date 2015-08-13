1e6b188de16efd747ace6773bd5fc5d9c7b6e771
Joao Moreno <mail@joaomoreno.com>
2015-08-13 17:19:48 +0200

    fix tests

--- test/download.test.js
+++ test/download.test.js
@@ -8,35 +8,6 @@ describe('download', function () {
 	this.timeout(1000 * 60 * 5);
 
 	it('should work', function(cb) {
-		download({ version: '0.19.5', platform: 'darwin' }, function (err, assetPath) {
-			assert(!err);
-			assert(fs.existsSync(assetPath));
-			cb();
-		});
-	});
-});
-
-describe('electron', function () {
-	this.timeout(1000 * 60 * 5);
-
-	it('should expose download', function(cb) {
-		var didSeeInfoPList = false;
-
-		electron
-			.download({ version: '0.19.5', platform: 'darwin' })
-			.on('data', function (f) {
-				if (f.relative === path.join('Atom.app', 'Contents', 'Info.plist')) {
-					didSeeInfoPList = true;
-				}
-			})
-			.on('error', cb)
-			.on('end', function () {
-				assert(didSeeInfoPList);
-				cb();
-			});
-	});
-	
-	it('should download electron', function(cb) {
 		var didSeeInfoPList = false;
 
 		electron
