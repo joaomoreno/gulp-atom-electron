444e74895d0fd6c26da1d11225b1a7e5dbdb2150
Shelley Vohr <shelley.vohr@gmail.com>
2020-04-21 21:44:07 -0700

    refactor: switch to @electron/get

--- src/download.js
+++ src/download.js
@@ -1,11 +1,7 @@
 'use strict';
 
-var os = require('os');
 var path = require('path');
-var fs = require('fs');
-var mkdirp = require('mkdirp');
-var GitHub = require('github-releases-ms');
-var ProgressBar = require('progress');
+const { downloadArtifact } = require('@electron/get');
 var semver = require('semver');
 var rename = require('gulp-rename');
 var es = require('event-stream');
@@ -13,112 +9,46 @@ var zfs = require('gulp-vinyl-zip');
 var filter = require('gulp-filter');
 var assign = require('object-assign');
 
-var cachePath = path.join(os.tmpdir(), 'gulp-electron-cache');
-
-function cache(assetName, repo, onMiss, cb) {
-	var assetFolder = path.join(cachePath, repo);
-	mkdirp(assetFolder, function (err) {
-		if (err) { return cb(err); }
-
-		var assetPath = path.join(assetFolder, assetName);
-
-		fs.exists(assetPath, function (exists) {
-			if (exists) { return cb(null, assetPath); }
-
-			var tempAssetPath = assetPath + '.tmp';
-			onMiss(tempAssetPath, function (err) {
-				if (err) { return cb(err); }
-
-				fs.rename(tempAssetPath, assetPath, function (err) {
-					if (err) { return cb(err); }
-
-					cb(null, assetPath);
-				});
-			});
-		});
-	});
-}
-
 function download(opts, cb) {
-	var repo = opts.repo || 'atom/electron';
-	var github = new GitHub({ repo: repo, token: opts.token });
-
 	if (!opts.version) {
 		return cb(new Error('Missing version'));
 	}
 
-	var platform = opts.platform;
-	if (!platform) {
+	if (!opts.platform) {
 		return cb(new Error('Missing platform'));
 	}
 
-	var arch = opts.arch;
-
+	let arch = opts.arch;
 	if (!arch) {
-		switch (platform) {
+		switch (opts.platform) {
 			case 'darwin': arch = 'x64'; break;
 			case 'win32': arch = 'ia32'; break;
 			case 'linux': arch = 'ia32'; break;
 		}
 	}
 
-	var version = 'v' + opts.version;
-	var assetName = [opts.assetName, version, platform, arch].join('-') + '.zip';
+	const downloadOptions = {
+		version: opts.version,
+		platform: opts.platform,
+		arch,
+		artifactName: opts.assetName,
+	};
 
-	function download(assetPath, cb) {
-		github.getReleases({ tag_name: version }, function (err, releases) {
-			if (err) { return cb(err); }
+	(opts.repo ? getDownloadUrl(opts.repo, downloadOptions) : Promise.resolve({})).then(({
+		err, downloadURL
+	}) => {
+		if (err) return cb(err)
 
-			var release = releases[0];
+		if (downloadURL) {
+			downloadOptions['mirrorOptions'] = { mirror: downloadUrl }
+		}
 
-			if (!release) {
-				return cb(new Error('No release ' + opts.version + ' found'));
-			}
-
-			var asset = release.assets.filter(function (asset) {
-				return asset.name === assetName;
-			})[0];
-
-			if (!asset) {
-				return cb(new Error('No asset for version ' + opts.version + ', platform ' + platform + ' and arch ' + arch + ' found'));
-			}
-
-			github.downloadAsset(asset, function (err, istream) {
-				if (err) {
-					console.error('Error creating download stream to download ' + asset.name);
-					return cb(err);
-				}
-
-				if (process.stdout.isTTY && !opts.quiet) {
-					var bar = new ProgressBar('↓ ' + asset.name + ' [:bar] :percent', {
-						total: asset.size,
-						width: 20
-					});
-
-					istream.on('data', function (chunk) { bar.tick(chunk.length); });
-				} else {
-					console.log('Downloading ' + asset.name + '...');
-				}
-
-				var ostream = fs.createWriteStream(assetPath);
-				istream.pipe(ostream);
-				istream.on('error', function (err) {
-					console.error('Error in input stream while downloading ' + asset.name);
-					cb(err);
-				});
-				ostream.on('error', function (err) {
-					console.error('Error in output stream while downloading ' + asset.name);
-					cb(err);
-				});
-				ostream.on('close', function () {
-					console.log('Downloaded ' + asset.name);
-					cb();
-				});
-			});
+		downloadArtifact(downloadOptions).then(zipFilePath => {
+			return cb(null, zipFilePath)
+		}).catch(err => {
+			return cb(err); 
 		});
-	}
-
-	cache(assetName, repo, download, cb);
+	});
 }
 
 function getDarwinLibFFMpegPath(opts) {
--- src/util.js
+++ src/util.js
@@ -1,5 +1,53 @@
 'use strict';
 
-exports.startsWith = function (haystack, needle) {
+const { Octokit } = require('@octokit/rest');
+
+function startsWith (haystack, needle) {
 	return haystack.substr(0, needle.length) === needle;
 };
+
+async function getDownloadUrl(repo, { version, platform, arch, token }) {
+	const [owner, repository] = repo.split('/');
+	const octokit = new Octokit({ auth: `token ${token}`});
+
+	const { data: release } = await octokit.repos.getReleaseByTag({
+		owner,
+		repository,
+		tag: version,
+	});
+
+	if (!release) {
+		return { error: `Release for ${version} not found` }
+	}
+
+	const { data: assets } = await octokit.repos.listAssetsForRelease({
+		owner,
+		repository,
+		release_id: release.id,
+	});
+
+	const asset = assets.find(asset => {
+		const targetName = `electron-${version}-${platform}-${arch}.zip`;
+		return asset.name === targetName;
+	});
+
+	if (!asset) {
+		return { error: `Release asset for ${version} not found` }
+	}
+
+	const { headers: { location }} = await octokit.repos.getReleaseAsset({
+		owner,
+		repository,
+		asset_id: asset.id,
+		headers: {
+			accept: 'application/octet-stream'
+		}
+	});
+
+	return { error: null, location };
+}
+
+module.exports = {
+	getDownloadUrl,
+	startsWith
+}
